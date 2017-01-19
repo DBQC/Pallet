2402ee4d0e941e68a4cdaeb6b537fd46d87bbfd9
cstephens <cstephens@ciphr.com>
2017-01-19 09:36:25 +0000

    Rebase test class added.

---
+++ Monopoly/Monopoly/RebaseTestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class RebaseTestClass
+    {
+        //This class was added and checked into a branch that is now far beyhind master. Rebase should preserve this file
+    }
+}
